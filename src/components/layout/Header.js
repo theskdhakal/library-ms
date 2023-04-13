e2fc61e85c20168e9a062cef6863101937feede8
Shiva Dhakal <theskdhakal@gmail.com>
2023-04-13 20:20:41 +1000

    some css updated

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,21 +4,23 @@ import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
 import { GoSignIn } from "react-icons/go";
 import { FaUserEdit } from "react-icons/fa";
-
+import logo from "../../Assets/logo.JPG";
 export const Header = () => {
   return (
-    <Navbar bg="dark" variant="dark" expand="md">
-      <Container>
+    <Navbar bg="success" variant="dark" expand="md">
+      <Container className="d-flex justify-content-between">
         <Navbar.Brand>
-          <Link to="/">KK</Link>
+          <Link to="/">
+            <img src={logo} alt="" className="image " />
+          </Link>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Link href="/signin" className="nav-link">
+            <Link to="/signin" className="nav-link">
               <GoSignIn className="fs-3" />
             </Link>
-            <Link href="/signup" className="nav-link">
+            <Link to="/signup" className="nav-link">
               <FaUserEdit className="fs-3" />
             </Link>
           </Nav>
